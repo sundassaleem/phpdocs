9923736572644e45400205d93d9d8a063dd8f4a4
sundas <sundassaleem678@gmail.com>
2019-01-09 09:29:29 +0500

    new changes

--- PHP/login/index.php
+++ PHP/login/index.php
@@ -8,10 +8,10 @@ if(!isset($_SESSION['Login'])){
 <!doctype html>	
 <html>
 	<head>
-	<title>Admin Panel Inteface</title>
+	<title>githib</title>
 	</head>
 	<body>
-	<h1>Welcome to admin panel</h1>
+	<h1>first heading</h1>
 	<a href="logout.php">Logout</a>
 	</body>
 </html>
